eb0c4c565242b0ea9e37ba3b831c5180d078ff52
Risdianita Ayu Ratnasari <riezdy.nitnat2@gmail.com>
2013-10-22 23:06:21 +0700

    Risdianita Ayu Ratnasari
    Modul 3

---
+++ modul 3/lat1_cookie.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>Set Cookie</title>
+</head>
+<body>
+
+<?php
+setcookie('nama_cookie', 'nilai_cookie');
+
+echo $_COOKIE ['nama_cookie'];
+
+?>
+<p>
+Tekan Refresh (F5);
+
+</body>
+
+</html>
