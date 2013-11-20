da1cbe29745115bb208e444111d8addf64e26979
Risdianita Ayu Ratnasari <riezdy.nitnat2@gmail.com>
2013-11-20 21:18:28 +0700

    Risdianita Ayu Ratnasari
    Modul5

---
+++ modul5/tugas.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Tugas Praktikum</title>
+</head>
+<body>
+
+	<table border="1" width="500px">
+
+		<?php
+			require "koneksi.php";
+			$query1 = "SELECT * FROM mahasiswa ORDER BY nim ";
+			$urut = 'asc';
+			$urutbaru = 'asc';
+			if(isset($_GET['orderby'])){
+				$orderby=$_GET['orderby'];
+				$urut=$_GET['urut'];
+				
+				$query1="SELECT * FROM  mahasiswa order by $orderby $urut ";
+				if($urut=='asc'){
+					$urutbaru='desc';					
+				}else{
+					$urutbaru='asc';
+				}
+			}
+		?>
+		<th>
+			<td><a href='tugas1-1.php?orderby=nim&urut=<?=$urutbaru;?>'>Nim</a></td>
+			<td><a href='tugas1-1.php?orderby=nama&urut=<?=$urutbaru;?>'>Nama</a></td>
+			<td><a href='tugas1-1.php?orderby=alamat&urut=<?=$urutbaru;?>'>Alamat</a></td>
+		</th>
+							
+		<?php
+			$result = mysql_query($query1) or die (mysql_error());
+			$no = 1; 
+			while($rows=mysql_fetch_object($result)){
+		?>
+
+		<tr>
+			<td><?php echo $no ?></td>
+			<td><?php echo $rows -> nim;?></td>
+			<td><?php echo $rows -> nama;?></td>
+			<td><?php echo $rows -> alamat;?></td>
+		</tr>
+		
+		<?php
+			$no++;
+			}
+		?>
+
+	</table>
+</body>
+</html>
