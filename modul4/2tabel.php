215ada3281527606d68a37c0afecf8614ef9302a
Risdianita Ayu Ratnasari <riezdy.nitnat2@gmail.com>
2013-11-20 18:52:51 +0700

    Risdianita Ayu Ratnasari
    Modul 4

---
+++ modul4/2tabel.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+	<title>Menciptakan Tabel</title>
+</head>
+
+<body>
+
+<?php
+require_once './koneksi.php';
+
+$sql = 'CREATE TABLE mahasiswa(
+		nim VARCHAR(12) NOT NULL,
+		nama VARCHAR(40) NOT NULL,
+		alamat VARCHAR(100),
+		PRIMARY KEY(nim)
+		) ENGINE=MyISAM;';
+
+$res = mysql_query($sql);
+if($res){
+	echo 'Tabel Created';
+	mysql_close($res);
+} else {
+  echo mysql_error();
+}
+?>
+
+</body>
+</html>
---
+++ modul4/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+	<head> 
+		<title>Akses dan Manipulasi Data</title>
+		<style type="text/css">
+		.even {background: #ddd;}
+		</style>
+	</head>
+
+	<body> 
+	<?php
+	ini_set('display_errors',1);
+
+	require_once './koneksi.php';
+	require_once './data_handler.php';
+
+	define('MHS','mahasiswa');
+
+	data_handler('?m=data');
+	?>
+	</body>
+</html>
